96ca3cf7aeb6d3e5ac5e839ca5e48efa1e159d3f
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-11-17 14:54:17 +0800

    Increase maximumTime if increment exists. (3) Bench: 4239512

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -98,7 +98,9 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
 
   if (limits.inc[us] > 0) // if increment exists
   {
-    maximumTime = TimePoint(std::min(0.85 * limits.time[us] - moveOverhead, maxScale * optimumTime));
+    maximumTime = TimePoint(std::min(std::max(std::clamp(0.8 + 0.01 * log10(limits.time[us]) / log10(100), 0.8, 0.85)
+            , std::clamp(0.85 - 0.01 * log10(limits.time[us]) / log10(100), 0.8, 0.85))
+            * limits.time[us] - moveOverhead, maxScale * optimumTime));
   }
   else // no increment
   {
