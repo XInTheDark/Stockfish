547132a47330e68b04c56ba9d037879f90ec2caa
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-11-24 20:20:54 +0800

    TUNE: nnueComplexity (1) Bench: 4390318

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1071,10 +1071,14 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  416 * nnueComplexity
-                        + 424 * abs(psq - nnue)
-                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 0)
-                        ) / 1024;
+      int c1 = 416, c2 = 424, c3 = 1024;
+      TUNE(SetRange(300, 600), c1, c2);
+      TUNE(SetRange(750, 1500), c3);
+
+      nnueComplexity = (  c1 * nnueComplexity
+                          + c2 * abs(psq - nnue)
+                          + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 0)
+                       ) / c3;
 
       // Return hybrid NNUE complexity to caller
       if (complexity)
