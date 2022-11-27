ba88206629ece582cf815e2d1ae8450d56c24126
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-11-27 18:00:32 +0800

    Adjust timeMan (variation 1) Bench: 4390318

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -73,7 +73,9 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
 
   // A user may scale time usage by setting UCI option "Slow Mover"
   // Default is 100 and changing this value will probably lose elo.
-  timeLeft = slowMover * timeLeft / 100;
+  timeLeft = (int) (
+          (97 + log10(timeLeft / 1000000) / log10(1.25)) / 100 * slowMover * timeLeft / 100
+    );
 
   // x basetime (+ z increment)
   // If there is a healthy increment, timeLeft can exceed actual available
