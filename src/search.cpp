f56480b7005378b51c7a3a0e8b1efdd8afaf6d22
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-10 08:31:39 +0800

    idk why I did this... (1) Bench: 1571960

--- src/search.cpp
+++ src/search.cpp
@@ -1051,6 +1051,8 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
+              newDepth -= 1;
+
               Value singularBeta = ttValue - (3 + (ss->ttPv && !PvNode)) * depth;
               Depth singularDepth = (depth - 1) / 2;
 
