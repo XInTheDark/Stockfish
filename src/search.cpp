d522ca93124054ef634183423d9c120c54a32a94
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-13 12:26:18 +0800

    Merge do_shallow_former_pv_00. Green STC, Yellow LTC. Bench: 1788275

--- src/search.cpp
+++ src/search.cpp
@@ -1241,7 +1241,7 @@ moves_loop: // When in check, search starts here
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
-              const bool doShallowerSearch = value < bestValue + newDepth;
+              const bool doShallowerSearch = value < bestValue + (2 - (ss->ttPv && !PvNode)) * newDepth;
 
               newDepth += doDeeperSearch - doShallowerSearch;
 
