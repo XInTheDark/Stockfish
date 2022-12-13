6cf83ff3701ba7d91c22f48b0adc0d3f8074d4c4
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-13 12:53:52 +0800

    Make slight adjustments to pvNode||cutNode. Bench: 2040225

--- src/search.cpp
+++ src/search.cpp
@@ -1433,9 +1433,7 @@ moves_loop: // When in check, search starts here
         //Assign extra bonus if current node is PvNode or cutNode
         //or fail low was really bad
         bool extraBonus =    PvNode
-                          || cutNode
-                          || ttMove
-                          || bestValue < alpha - 62 * depth;
+                          || cutNode;
 
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
     }
