cd86e2ec91f4458c0ca7c2d7a61a163a5aa7170e
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-14 18:30:37 +0800

    Try capture instead of check. (2.2) Bench: 4275422

--- src/search.cpp
+++ src/search.cpp
@@ -1358,7 +1358,7 @@ moves_loop: // When in check, search starts here
         //or fail low was really bad
         bool extraBonus =    PvNode
                           || cutNode
-                          || givesCheck;
+                          || capture;
 
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
     }
