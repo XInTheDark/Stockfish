9ccf55e48a4c8bb753f330d94ffc50b26a435ffb
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-14 11:43:19 +0800

    Take 3 Bench: 3766509

--- src/search.cpp
+++ src/search.cpp
@@ -1351,13 +1351,12 @@ moves_loop: // When in check, search starts here
                          quietsSearched, quietCount, capturesSearched, captureCount, depth);
 
     // Bonus for prior countermove that caused the fail low
-    else if (   (depth >= 5 || PvNode || bestValue < alpha - 62 * depth)
+    else if (   (depth >= 5 || PvNode || cutNode || bestValue < alpha - 62 * depth)
              && !priorCapture)
     {
         //Assign extra bonus if current node is PvNode or cutNode
         //or fail low was really bad
         bool extraBonus =    PvNode
-                          || cutNode
                           || givesCheck;
 
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth) * (1 + extraBonus));
