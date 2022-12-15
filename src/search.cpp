cca6a1534f9a132d617522a0c5557c3ac7b8f4f5
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-15 14:26:01 +0800

    Revert more_reduce_002_002. Might be the cause of poor performance.
    Bench: 3129926

--- src/search.cpp
+++ src/search.cpp
@@ -1395,7 +1395,7 @@ moves_loop: // When in check, search starts here
 
               // Reduce other moves if we have found at least one score improvement
               if (depth > 1
-                  && depth < 13
+                  && depth < 6
                   && beta  <  VALUE_KNOWN_WIN
                   && alpha > -VALUE_KNOWN_WIN)
                   depth -= 1 + (depth > 2 && moreReduce);
