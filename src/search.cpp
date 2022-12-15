bc4865c8aa846d617bc586824edb1685021369dc
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-15 19:29:47 +0800

    Fix parenthesis, take 1 Bench: 4035994

--- src/search.cpp
+++ src/search.cpp
@@ -892,7 +892,7 @@ namespace {
     // Use qsearch if depth is equal or below zero (~4 Elo)
     if (    PvNode
         && !ttMove)
-        depth -= 3 - depth <= 9;
+        depth -= 3 - (depth <= 9);
 
     if (depth <= 0)
         return qsearch<PV>(pos, ss, alpha, beta);
