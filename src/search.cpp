9671a52bfb43b751425baf7b3570919be27f6acb
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-20 09:45:25 +0800

    Merge lateRed1c (passing STC by a lot) https://tests.stockfishchess.org/tests/view/63a0e95c264a0cf18f86fc3d
    Bench: 2645828

--- src/search.cpp
+++ src/search.cpp
@@ -1293,7 +1293,7 @@ moves_loop: // When in check, search starts here
       // Step 18. Full depth search when LMR is skipped
       else if (!PvNode || moveCount > 1)
       {
-              value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
+              value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 3 ? r / 4 : 0), !cutNode);
       }
 
       if (value > beta)
