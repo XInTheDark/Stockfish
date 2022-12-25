c3a7d840663084413a752bbd9bc654531f1594d2
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-25 19:50:44 +0800

    Aggressive window Take 4 Bench: 3810223

--- src/search.cpp
+++ src/search.cpp
@@ -398,7 +398,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  beta = beta > 2 * alpha ? (alpha + beta) / 2 : alpha;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
