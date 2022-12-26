10c36c95b8c8c177576198d97442aeddb1240290
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-26 20:40:12 +0800

    Take 4.4: dss only when not improving Bench: 3409877

--- src/search.cpp
+++ src/search.cpp
@@ -1191,7 +1191,7 @@ moves_loop: // When in check, search starts here
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
-              const bool doEvenShallowerSearch = value > alpha && value < alpha + 50;
+              const bool doEvenShallowerSearch = value > alpha && value < alpha + 50 && !improving;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
