e6d3c3013684565ff92a1d911797953472df5129
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-26 14:20:22 +0800

    Take 1: -27 Bench: 3973244

--- src/search.cpp
+++ src/search.cpp
@@ -835,7 +835,7 @@ namespace {
         }
     }
 
-    probCutBeta = beta + 191 - 54 * improving;
+    probCutBeta = beta + 164;
 
     // Step 10. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
