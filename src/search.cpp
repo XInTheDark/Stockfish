280aa85835b116564a8bca8a79db5c9bf7ce017e
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-27 07:22:08 +0800

    Take 1: deds only when improving Bench: 3776867

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop: // When in check, search starts here
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
+              const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5 && improving;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
