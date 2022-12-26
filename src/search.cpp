3c4871d7eba531968e9a48766733eae08b01955a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-26 14:41:12 +0800

    Take 1 Bench: 3736900

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
       if (    depth >= 2
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
-              || !capture
+              || (!capture && !givesCheck)
               || (cutNode && (ss-1)->moveCount > 1)))
       {
           // In general we want to cap the LMR depth search at newDepth, but when
