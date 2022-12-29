e300c51daef5872c68b4b36338367166aaa8301c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-29 08:44:53 +0800

    Take 4 Bench: 3492734

--- src/search.cpp
+++ src/search.cpp
@@ -837,7 +837,7 @@ namespace {
         }
     }
 
-    probCutBeta = beta + 191 - 54 * improving - 27 * more_improving;
+    probCutBeta = beta + 191 - 27 * improving - 27 * more_improving;
 
     // Step 10. ProbCut (~10 Elo)
     // If we have a good enough capture and a reduced search returns a value
@@ -988,7 +988,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-          moveCountPruning = moveCount >= futility_move_count(improving + more_improving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving + more_improving, depth, moveCount, delta, thisThread->rootDelta), 0);
