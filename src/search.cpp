363f0dcbc299008fd67f924646b78b621d854f66
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-12-31 15:58:06 +0800

    Take 1: more DSS for LMR Bench: 3873214

--- src/search.cpp
+++ src/search.cpp
@@ -1190,7 +1190,7 @@ moves_loop: // When in check, search starts here
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
-              const bool doShallowerSearch = value < bestValue + newDepth;
+              const bool doShallowerSearch = value < bestValue + newDepth || value < beta - 200;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
