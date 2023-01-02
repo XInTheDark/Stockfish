9a01da5c5f8b79e6f0f6bd99be5d62d28210d7c3
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-02 13:36:34 +0800

    dess Take 4: Stricter dess Bench: 3365554

--- src/search.cpp
+++ src/search.cpp
@@ -1191,7 +1191,7 @@ moves_loop: // When in check, search starts here
               const bool doDeeperSearch = value > (alpha + 64 + 11 * (newDepth - d));
               const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + 2 * newDepth;
-              const bool doEvenShallowerSearch = doShallowerSearch && (value < beta - 256 || value < alpha + 256);
+              const bool doEvenShallowerSearch = doShallowerSearch && (value < beta - 256 || value < alpha + 128);
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
