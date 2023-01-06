507e6f08103ab1b4bfb4c7ed5e9ed853ed8b2d74
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-06 19:58:09 +0800

    Take 1 Bench: 4105350

--- src/search.cpp
+++ src/search.cpp
@@ -1160,6 +1160,13 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
+      // Increase reduction if ttMove is a dangerous pawn push
+      if (   depth >= 6
+             && ttMove != MOVE_NONE
+             && type_of(pos.piece_on(from_sq(ttMove))) == PAWN
+             && relative_rank(us, to_sq(ttMove)) >= RANK_5)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
