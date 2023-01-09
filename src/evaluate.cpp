d123e7f35c39c15055497f747648b249bb02756a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-09 20:45:32 +0800

    Take 4 Bench: 3906917

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1087,6 +1087,10 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   // Damp down the evaluation linearly when shuffling
   v = v * (200 - pos.rule50_count()) / 214;
 
+  // Damp down the evaluation even more if we are in a drawish position
+  if (pos.rule50_count() > 50)
+      v = v * (100 - pos.rule50_count()) / 100;
+
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
