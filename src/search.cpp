568620ab045d6176002424e1393808ee5be1e561
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-11 17:03:58 +0800

    dess Take 2.2 (/3?) Bench: 3946685

--- src/search.cpp
+++ src/search.cpp
@@ -1195,7 +1195,7 @@ moves_loop: // When in check, search starts here
               const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
               const bool doEvenShallowerSearch = doShallowerSearch && cutNode
-                      && value < (bestValue + newDepth) / 2;
+                      && value < (bestValue + 2 * newDepth) / 3;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
 
