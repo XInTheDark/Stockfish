5db3283ea343a7d6c9eaf26caed38cd8678ff5d2
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-21 18:55:06 +0800

    Take 1? Bench: 4046529

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning (~11 Elo)
-              if (!pos.see_ge(move, Value(-220) * depth))
+              if (depth < 9 && !pos.see_ge(move, Value(-220) * (depth)))
                   continue;
           }
           else
