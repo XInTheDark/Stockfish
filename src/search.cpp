638dda26fde70d2da365481b66ebd56bd9964abb
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-26 20:38:23 +0800

    Take 1 Bench: 4060034

--- src/search.cpp
+++ src/search.cpp
@@ -1164,6 +1164,10 @@ moves_loop: // When in check, search starts here
           && (mp.threatenedPieces & from_sq(move)))
           r--;
 
+      // Decrease reduction if in check and reduction is high
+      if (depth > 6 && r >= 2 && ss->inCheck)
+          r--;
+
       // Increase reduction if next ply has a lot of fail high
       if ((ss+1)->cutoffCnt > 3)
           r++;
