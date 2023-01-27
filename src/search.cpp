7dc8d1725aa506410bff8d8056232445d901ea95
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-27 16:40:57 +0800

    r++ if bad history Bench: 3565318

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,10 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
+      // Increase reduction if move has a bad history
+      if ((*contHist[0])[movedPiece][to_sq(move)] < -(2000 + 50 * depth + 1000 * PvNode))
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
