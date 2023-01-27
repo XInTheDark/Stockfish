a689d705bc25c27f25ba038252a0b20f43bcbe85
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-01-27 16:34:06 +0800

    Similar idea to reduction_killer Bench: 4063605

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,11 @@ moves_loop: // When in check, search starts here
       if ((ss+1)->cutoffCnt > 3)
           r++;
 
+      // Decrease reduction if move has a good history
+      if ((*contHist[0])[movedPiece][to_sq(move)] >= 4000
+          && PvNode)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
