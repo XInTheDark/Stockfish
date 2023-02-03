5d326130a41e882926c711bfc8bf3ee3d9591e1a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-03 19:56:37 +0800

    try original idea 3 Bench: 3789905

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,8 @@ moves_loop: // When in check, search starts here
 
       // Decrease reduction if move is a killer and we have a good history
       if (move == ss->killers[0]
-          && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
+          && (*contHist[0])[movedPiece][to_sq(move)] >= 5600
+          && move != ttMove)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
