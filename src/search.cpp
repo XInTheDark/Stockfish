b67b473dcf26ea41d04f5ebbf4b7d4b4f451647d
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-04 21:12:21 +0800

    reduce for all killer moves. Bench: 3967428

--- src/search.cpp
+++ src/search.cpp
@@ -1176,6 +1176,9 @@ moves_loop: // When in check, search starts here
       if (move == ss->killers[0]
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
           r--;
+      else if (move == ss->killers[1]
+               && (*contHist[0])[movedPiece][to_sq(move)] >= 5600)
+          r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
