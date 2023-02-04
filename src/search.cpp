e010dc5bc5fc1b972950fa8e0bb7e416a7ee818c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-04 13:38:56 +0800

    Take 1 Bench: 4508037

--- src/search.cpp
+++ src/search.cpp
@@ -1177,6 +1177,10 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
           r--;
 
+      // Decrease reduction if rule50 counter is high
+      if (depth > 9 && improvement < 50)
+          r -= pos.rule50_count() / 20;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
