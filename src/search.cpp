7ff060118b98280afba55ed5930976ce19536a3e
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-04 14:28:06 +0800

    Take 2 Bench: 3920396

--- src/search.cpp
+++ src/search.cpp
@@ -1159,6 +1159,9 @@ moves_loop: // When in check, search starts here
       if (PvNode)
           r -= 1 + 11 / (3 + depth);
 
+      if (rootNode && !bestMove)
+          r -= 1 + (depth < 9);
+
       // Decrease reduction if ttMove has been singularly extended (~1 Elo)
       if (singularQuietLMR)
           r--;
