29b981cdc1bea442aac2b987dacc2eb83bdb3474
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-05 08:31:56 +0800

    Scale more on depth Bench: 4046378

--- src/search.cpp
+++ src/search.cpp
@@ -1563,7 +1563,8 @@ moves_loop: // When in check, search starts here
 
       // Do not search moves with bad enough SEE values (~5 Elo)
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && !pos.see_ge(move, Value(-108)))
+          && !pos.see_ge(move, Value(std::max(-250 - depth * 15, -700)))
+          )
           continue;
 
       // Speculative prefetch as early as possible
