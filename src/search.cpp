8b5ab8fe786ebed1a21310f10565d97110740dff
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-05 14:46:53 +0800

    Take 1 Bench: 4279753

--- src/search.cpp
+++ src/search.cpp
@@ -1177,6 +1177,11 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
           r--;
 
+      // Decrease reduction if eval is high and we have a good history
+      if (ss->staticEval > 300
+          && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
