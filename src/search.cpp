b9b1fd45a9c596fde7a296188e774fe60b9ef8e3
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-08 18:40:37 +0800

    Take 1: SEE reduction Bench: 4318700

--- src/search.cpp
+++ src/search.cpp
@@ -1177,6 +1177,10 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
           r--;
 
+      // Decrease reduction if move has good SEE value
+      if (PvNode && pos.see_ge(move, Value(108 + 6 * depth)))
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
