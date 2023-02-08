3b758b674d73e483e08a90dc9879ab938bc4e5e3
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-08 18:47:36 +0800

    deds for killer move Bench: 3996333

--- src/search.cpp
+++ src/search.cpp
@@ -1209,7 +1209,7 @@ moves_loop: // When in check, search starts here
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
               const bool doDeeperSearch = value > (alpha + 66 + 11 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 582 && ss->doubleExtensions <= 5;
+              const bool doEvenDeeperSearch = (value > alpha + 582 || move == ss->killers[0]) && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
