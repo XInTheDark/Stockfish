5b3cf7cbb016cd33280478826432088ddc2f5b7f
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-11 10:22:38 +0800

    Take 1: TT optimism Bench: 3959302

--- src/tt.h
+++ src/tt.h
@@ -40,10 +40,11 @@ struct TTEntry {
   Move  move()  const { return (Move )move16; }
   Value value() const { return (Value)value16; }
   Value eval()  const { return (Value)eval16; }
+  Value optimism() const { return (Value)optimism16; }
   Depth depth() const { return (Depth)depth8 + DEPTH_OFFSET; }
   bool is_pv()  const { return (bool)(genBound8 & 0x4); }
   Bound bound() const { return (Bound)(genBound8 & 0x3); }
-  void save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev);
+  void save(Key k, Value v, bool pv, Bound b, Depth d, Move m, Value ev, Value opt);
 
 private:
   friend class TranspositionTable;
@@ -54,6 +55,7 @@ private:
   uint16_t move16;
   int16_t  value16;
   int16_t  eval16;
+  int16_t optimism16;
 };
 
 
@@ -72,7 +74,7 @@ class TranspositionTable {
     char padding[2]; // Pad to 32 bytes
   };
 
-  static_assert(sizeof(Cluster) == 32, "Unexpected Cluster size");
+  static_assert(sizeof(Cluster) == 38, "Unexpected Cluster size");
 
   // Constants used to refresh the hash table periodically
   static constexpr unsigned GENERATION_BITS  = 3;                                // nb of bits reserved for other things
