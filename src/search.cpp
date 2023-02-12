b5b78fd027c4bcc3fab7a577e336c4a942783e27
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-12 18:36:45 +0800

    Take 1 Bench: 3719742

--- src/search.cpp
+++ src/search.cpp
@@ -401,7 +401,9 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  // We increase aspiration window by 1% for each increase in depth
+                  double alphaPercentage = std::min(0.5 + 0.01 * (completedDepth - 1), 0.99);
+                  beta = Value( (double) alpha * alphaPercentage + (double) beta * (1 - alphaPercentage) );
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
