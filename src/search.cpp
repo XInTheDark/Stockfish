94541c5fb9db064c9e8187e63c62853fbd300b9e
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-12 17:42:23 +0800

    simply remove `deltaV3` and `deltaV4` Bench: 3841998
    ```
    deltaV1,10,-50,50,5,0.0020
    t,512,1,1023,51.1,0.0020
    deltaV2,15400,0,30800,1540,0.0020
    opt1,116,0,232,11.6,0.0020
    opt2,170,0,340,17,0.0020
    ```

--- src/search.cpp
+++ src/search.cpp
@@ -261,13 +261,11 @@ void MainThread::search() {
 
 int deltaV1 = 10, deltaV2 = 15400,
     opt1 = 116, opt2 = 170,
-    t = 512,
-    deltaV3 = 256, deltaV4 = 2;
+    t = 512;
 
 TUNE(SetRange(-50, 50), deltaV1);
 TUNE(SetRange(1, 1023), t);
-TUNE(deltaV2, opt1, opt2, deltaV3);
-TUNE(SetRange(-50, 50), deltaV4);
+TUNE(deltaV2, opt1, opt2);
 
 void Thread::search() {
 
@@ -426,7 +424,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta * deltaV3 / 1024 + deltaV4;
+              delta += delta / 4 + 2;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
