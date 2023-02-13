67e738ccce79b7abd5767832c8b8222721cc5f6d
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-13 21:44:17 +0800

    Try +5%? Bench: 3963042

--- src/search.cpp
+++ src/search.cpp
@@ -401,7 +401,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  beta = (alpha * 460 + beta * 564) / 1024;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
