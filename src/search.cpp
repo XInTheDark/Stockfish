83ed3ac2e0b796ad9760cd3461c0abd583155426
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-02-15 12:09:32 +0800

    Take 1 Bench: 4162828

--- src/search.cpp
+++ src/search.cpp
@@ -1175,6 +1175,9 @@ moves_loop: // When in check, search starts here
           && (*contHist[0])[movedPiece][to_sq(move)] >= 3600)
           r--;
 
+      if (move == ttMove && tte->is_pv() && tte->depth() >= depth - 5)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
