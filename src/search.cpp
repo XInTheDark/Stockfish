11f170d4e656328984306d50995b6992543225ca
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-03-07 14:49:38 +0800

    Quiet TT extension = 2 Bench: 4749360

--- src/search.cpp
+++ src/search.cpp
@@ -1111,17 +1111,17 @@ moves_loop: // When in check, search starts here
                   extension = -1;
           }
 
-          // Check extensions (~1 Elo)
-          else if (   givesCheck
-                   && depth > 10
-                   && abs(ss->staticEval) > 88)
-              extension = 1;
-
           // Quiet ttMove extensions (~1 Elo)
           else if (   PvNode
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5705)
+              extension = 2;
+
+          // Check extensions (~1 Elo)
+          else if (   givesCheck
+                   && depth > 10
+                   && abs(ss->staticEval) > 88)
               extension = 1;
       }
 
