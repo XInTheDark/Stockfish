bcb7c3e9fe2ec78b6fca5da5c7ad7fcbb697aeae
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-03-12 15:04:36 +0800

    ttv <= alpha, also -2 Bench: 4118911

--- src/search.cpp
+++ src/search.cpp
@@ -1112,7 +1112,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is less than alpha, we reduce it (negative extension)
               else if (ttValue <= alpha)
-                  extension = -1;
+                  extension = -2;
 
               if (extension < 0 && !PvNode)
                   extension--;
