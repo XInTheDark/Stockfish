72dd843b479bad0486ed99c926556b14bbeb9a1e
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-03-15 10:08:55 +0800

    Take 1 Bench: 4855000

--- src/search.cpp
+++ src/search.cpp
@@ -401,7 +401,8 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  // Adjust aspiration window
+                  beta = std::max((alpha + beta) / 2, bestValue + delta);
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
