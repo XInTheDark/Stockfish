0a6ab5fe6cdea1dccab9c0cd91eebedf821b9966
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-03-17 13:41:43 +0800

    Take 1 Bench: 4691788

--- src/search.cpp
+++ src/search.cpp
@@ -751,7 +751,8 @@ namespace {
 
         // ttValue can be used as a better position evaluation (~7 Elo)
         if (    ttValue != VALUE_NONE
-            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
+            && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER))
+            && tte->depth() > depth - 3)
             eval = ttValue;
     }
     else
