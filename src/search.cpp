93ca2d53eb4f43d8187b331c8af57914dcf2eddc
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-06 08:40:13 +0800

    Take 1 Bench: 4477443

--- src/search.cpp
+++ src/search.cpp
@@ -735,7 +735,7 @@ namespace {
     {
         // Never assume anything about values stored in TT
         ss->staticEval = eval = tte->eval();
-        if (eval == VALUE_NONE)
+        if (eval == VALUE_NONE || depth < 12)
             ss->staticEval = eval = evaluate(pos, &complexity);
         else // Fall back to (semi)classical complexity for TT hits, the NNUE complexity is lost
         {
