3542183ea6d1278110bb8fc12df4f13993e3ea8d
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-07 08:26:58 +0800

    Snipe of xoto's idea 1 Bench: 4682500

--- src/search.cpp
+++ src/search.cpp
@@ -364,8 +364,8 @@ void Thread::search() {
 
               // Adjust optimism based on root move's previousScore
               int opt = 120 * prev / (std::abs(prev) + 161);
-              optimism[ us] = Value(opt);
-              optimism[~us] = -optimism[us];
+              optimism[ us] = Value(std::max(opt, 0));
+              optimism[~us] = -opt;
           }
 
           // Start with a small aspiration window and, in the case of a fail
