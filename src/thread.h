9ec24b57ff1c25c349bcf639902c778fea8bad2e
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-09 10:07:23 +0800

    Simplify away optimism Bench: 3884925

--- src/thread.h
+++ src/thread.h
@@ -63,7 +63,7 @@ public:
   std::atomic<uint64_t> nodes, tbHits, bestMoveChanges;
   int selDepth, nmpMinPly;
   Color nmpColor;
-  Value bestValue, optimism[COLOR_NB];
+  Value bestValue;
 
   Position rootPos;
   StateInfo rootState;
