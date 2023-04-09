a9eede35802d906b273a4c7aaa2d58b21b6d4911
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-09 11:00:20 +0800

    Depth 10??? Bench: 3887273

--- src/search.cpp
+++ src/search.cpp
@@ -355,7 +355,7 @@ void Thread::search() {
           selDepth = 0;
 
           // Reset aspiration window starting size
-          if (rootDepth >= 6)
+          if (rootDepth >= 10)
           {
               Value prev = rootMoves[pvIdx].averageScore;
               delta = Value(10) + int(prev) * prev / 16502;
