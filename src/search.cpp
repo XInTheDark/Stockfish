46784679eb6b71726548c0c0c8a7f9170d26b415
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-24 13:41:57 +0800

    Take 1 Bench: 3929257

--- src/search.cpp
+++ src/search.cpp
@@ -1172,6 +1172,11 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      // Decrease reduction if move is capture and we have a good history
+      if (capture
+          && (*contHist[0])[movedPiece][to_sq(move)] >= 4000)
+          r--;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
