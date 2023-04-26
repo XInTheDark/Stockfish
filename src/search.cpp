29aac0bd75e4cb4c1b7852321ea4086110fab9d8
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-26 10:15:25 +0800

    Tuned 96.7k MTC
    Bench: 3714847

--- src/search.cpp
+++ src/search.cpp
@@ -456,14 +456,14 @@ void Thread::search() {
           && !Threads.stop
           && !mainThread->stopOnPonderhit)
       {
-          double fallingEval = (69 + 13 * (mainThread->bestPreviousAverageScore - bestValue)
-                                    +  6 * (mainThread->iterValue[iterIdx] - bestValue)) / 619.6;
-          fallingEval = std::clamp(fallingEval, 0.5, 1.5);
+          double fallingEval = (75 + 14 * (mainThread->bestPreviousAverageScore - bestValue)
+                                    +  6 * (mainThread->iterValue[iterIdx] - bestValue)) * 111 / 61960;
+          fallingEval = std::clamp(fallingEval, 0.389, 1.734);
 
           // If the bestMove is stable over several iterations, reduce time accordingly
-          timeReduction = lastBestMoveDepth + 8 < completedDepth ? 1.57 : 0.65;
-          double reduction = (1.4 + mainThread->previousTimeReduction) / (2.08 * timeReduction);
-          double bestMoveInstability = 1 + 1.8 * totBestMoveChanges / Threads.size();
+          timeReduction = lastBestMoveDepth + 9 < completedDepth ? 1.664 : 0.539;
+          double reduction = (1.583 + mainThread->previousTimeReduction) / (2.106 * timeReduction);
+          double bestMoveInstability = 0.975 + 2.031 * totBestMoveChanges / Threads.size();
 
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 
@@ -483,7 +483,7 @@ void Thread::search() {
                   Threads.stop = true;
           }
           else if (   !mainThread->ponder
-                   && Time.elapsed() > totalTime * 0.50)
+                   && Time.elapsed() > totalTime * 0.549)
               Threads.increaseDepth = false;
           else
               Threads.increaseDepth = true;
