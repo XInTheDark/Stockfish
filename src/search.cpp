a8ca7af626ffd8ef84e58a9aed011d017796a28d
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-29 19:27:11 +0800

    Re-evaluate TT pos at low depth (<12) Bench: 3404147

--- src/search.cpp
+++ src/search.cpp
@@ -722,7 +722,7 @@ namespace {
     {
         // Never assume anything about values stored in TT
         ss->staticEval = eval = tte->eval();
-        if (eval == VALUE_NONE)
+        if (eval == VALUE_NONE || tte->depth() < 12)
             ss->staticEval = eval = evaluate(pos);
         else
         {
