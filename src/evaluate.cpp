b07aab6bba6aaf519ff2cccdf7ad5937bab81c92
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-30 13:10:01 +0800

    Take 2: pc >= 7 instead Bench: 3447943

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1058,7 +1058,7 @@ Value Eval::evaluate(const Position& pos) {
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !useNNUE || (abs(psq) > 2 * scale && pos.count<ALL_PIECES>() >= 8);
+  bool useClassical = !useNNUE || (abs(psq) > 2 * scale && pos.count<ALL_PIECES>() >= 7);
 
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
