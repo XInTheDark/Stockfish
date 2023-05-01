266d469ea0179b996632dc28bae23147f37e4aac
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-01 20:13:06 +0800

    Take 3: less reduction Bench: 4011890

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts here
           r--;
 
       // Less reduction for null moves
-      if ((ss+1)->currentMove == MOVE_NULL)
+      if ((ss-1)->currentMove == MOVE_NULL)
           r--;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
