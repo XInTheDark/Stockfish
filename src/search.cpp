11ed4eaf6b33147d1450775676160cd0d3ccbdab
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-01 20:03:20 +0800

    Take 2: null move reduction: 1 + !Pv Bench: 3435360

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
 
       // More reduction for null moves
       if ((ss+1)->currentMove == MOVE_NULL)
-          r++;
+          r += 1 + !PvNode;
 
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
