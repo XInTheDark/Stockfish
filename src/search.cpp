aeae49ad9ddd947b01fc999e1397056501130ea5
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-06 08:40:20 +0800

    Null move search only if eval != 0 Bench: 3567096

--- src/search.cpp
+++ src/search.cpp
@@ -779,6 +779,7 @@ namespace {
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (   !PvNode
+        && eval
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 18755
         &&  eval >= beta
