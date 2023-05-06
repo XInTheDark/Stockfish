e7cb8ad860e22e58a18df0b111eeaba260243411
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-06 15:10:16 +0800

    cutNode = true Bench: 3400662

--- src/search.cpp
+++ src/search.cpp
@@ -817,7 +817,7 @@ namespace {
             // until ply exceeds nmpMinPly.
             thisThread->nmpMinPly = ss->ply + 3 * (depth-R) / 4;
 
-            Value v = search<NonPV>(pos, ss, beta-1, beta, depth-R, false);
+            Value v = search<NonPV>(pos, ss, beta-1, beta, depth-R, true);
 
             thisThread->nmpMinPly = 0;
 
