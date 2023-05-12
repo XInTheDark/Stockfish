3cdf552e7dcfcf4763dca5b4d5846877dfe51555
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-12 11:19:36 +0800

    Viz snipe tuned @ 120k LTC Bench: 3635553

--- src/search.cpp
+++ src/search.cpp
@@ -1201,8 +1201,8 @@ moves_loop: // When in check, search starts here
           {
               // Adjust full depth search based on LMR results - if result
               // was good enough search deeper, if it was bad enough search shallower
-              const bool doDeeperSearch = value > (bestValue + 68 + 12 * (newDepth - d));
-              const bool doEvenDeeperSearch = value > alpha + 588 && ss->doubleExtensions <= 5;
+              const bool doDeeperSearch = value > (bestValue + 62 + 726 * (newDepth - d) / 64);
+              const bool doEvenDeeperSearch = value > bestValue + 550 && ss->doubleExtensions <= 5;
               const bool doShallowerSearch = value < bestValue + newDepth;
 
               ss->doubleExtensions = ss->doubleExtensions + doEvenDeeperSearch;
