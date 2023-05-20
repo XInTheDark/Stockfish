040e55ed8572788075c7f55cd89246c0917a0608
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-20 21:34:10 +0800

    Take 2 Bench: 3320049

--- src/search.cpp
+++ src/search.cpp
@@ -394,7 +394,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = std::max(alpha, (alpha + beta) / 2 - delta);
+                  beta = std::max(alpha, (alpha + beta - delta) / 2);
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
