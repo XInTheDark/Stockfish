b483a0d0708166a3d6da148d16d47a416bbe7b82
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-21 11:21:34 +0800

    Take 6 Bench: 2957837

--- src/search.cpp
+++ src/search.cpp
@@ -394,7 +394,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = alpha + 1;
+                  beta = std::min(bestValue + delta, VALUE_INFINITE);
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
