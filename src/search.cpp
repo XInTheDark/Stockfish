67b211a25488ee27283c8c2c225e5eea7572dd83
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-21 11:18:14 +0800

    Retry aggWindow idea, alpha+1 Bench: 3005493

--- src/search.cpp
+++ src/search.cpp
@@ -394,7 +394,7 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = alpha + delta;
+                  beta = alpha + 1;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
