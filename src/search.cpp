405c42131d616e8a2cc05200bdacb8a3c9bba859
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-29 13:29:57 +0800

    idea 1: 30k games, Take 3 Bench: 2430624

--- src/search.cpp
+++ src/search.cpp
@@ -395,7 +395,7 @@ void Thread::search() {
               if (bestValue <= alpha)
               {
                   // Scale window size based on bestValue
-                  int alphaScale = 1042 - (int)bestValue * bestValue / 5444;
+                  int alphaScale = 1035 - (int)bestValue * bestValue / 5516;
                   alphaScale = std::clamp(alphaScale, 0, 2048);
 
                   beta = (alpha * alphaScale + beta * (2048 - alphaScale)) / 2048;
