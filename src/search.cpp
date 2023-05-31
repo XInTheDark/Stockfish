da8b4d76f2485506925bdb58d6132d0176809edb
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-05-31 13:48:18 +0800

    idea 2, Tuned @ 100k+ games Bench: 2857891

--- src/search.cpp
+++ src/search.cpp
@@ -394,7 +394,11 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
+                  // Scale window size based on depth
+                  int alphaScale = 1075 - (int)rootDepth * rootDepth / 291 - 3 * (int)rootDepth;
+
+                  alphaScale = std::clamp(alphaScale, 0, 2048);
+                  beta = (alpha * alphaScale + beta * (2048 - alphaScale)) / 2048;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
