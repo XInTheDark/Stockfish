c6befec719c71886554ec71400413cbe92e5ecab
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-06-12 13:11:13 +0800

    Take 2 Bench: 1801993

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,10 @@ moves_loop: // When in check, search starts here
       else if (move == ttMove)
           r--;
 
+      // Increase reduction if value is less than bestValue
+      if (value < bestValue + newDepth)
+          r++;
+
       ss->statScore =  2 * thisThread->mainHistory[us][from_to(move)]
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
