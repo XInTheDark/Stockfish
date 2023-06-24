73dd369e1dfdd19ce37d2eb3c9b2686fd33247ad
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-06-24 14:45:52 +0800

    Tuned @ 45k STC, test idea Bench: 2761162

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1042,11 +1042,6 @@ make_v:
 
 } // namespace Eval
 
-int a1 = 2500, a2 = 96, a3 = 482, a4 = 2048;
-TUNE(a1);
-TUNE(SetRange(-5120, 5120), a2);
-TUNE(SetRange(-5120, 5120), a3);
-TUNE(a4);
 
 /// evaluate() is the evaluator for the outer world. It returns a static
 /// evaluation of the position from the point of view of the side to move.
@@ -1062,7 +1057,7 @@ Value Eval::evaluate(const Position& pos) {
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
   const int gamePly = pos.game_ply();
-  const int nnueThreshold = std::max(a1 - a2 * gamePly * gamePly / 1024 - a3 * gamePly / 256, a4);
+  const int nnueThreshold = gamePly <= 32 ? 2650 - 533 * gamePly * gamePly / 1024 - 330 * gamePly / 256 : 2041;
   const bool useClassical = !useNNUE || abs(psq) > nnueThreshold;
 
   if (useClassical)
