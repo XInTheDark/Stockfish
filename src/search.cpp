bef6fec66b84b12a19434703b8a8b8ccfc703498
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-06-29 12:39:40 +0800

    v3 (?) Bench: 2653533

--- src/search.cpp
+++ src/search.cpp
@@ -1238,7 +1238,7 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
-          if (rootNode && depth < 16 && move != ttMove)
+          if (rootNode && improving && move != ttMove)
               newDepth++;
 
           value = -search<PV>(pos, ss+1, -beta, -alpha, newDepth, false);
