f7218c0044b56e8981b23e15d95957dfbed2a307
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-06-29 12:42:59 +0800

    Take 1 Bench: 3222725

--- src/search.cpp
+++ src/search.cpp
@@ -1238,6 +1238,9 @@ moves_loop: // When in check, search starts here
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
 
+          if (improving && move != ttMove)
+              newDepth++;
+
           value = -search<PV>(pos, ss+1, -beta, -alpha, newDepth, false);
       }
 
