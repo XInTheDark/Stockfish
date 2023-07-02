febcfb294a43c1edf0c6de5ca83779a70655485c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-07-02 13:36:56 +0800

    Take 1 Bench: 2755338

--- src/search.cpp
+++ src/search.cpp
@@ -1568,8 +1568,8 @@ moves_loop: // When in check, search starts here
 
             // Continuation history based pruning (~3 Elo)
             if (   !capture
-                && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < 0
-                && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < 0)
+                && (*contHist[0])[pos.moved_piece(move)][to_sq(move)] < 500
+                && (*contHist[1])[pos.moved_piece(move)][to_sq(move)] < 500)
                 continue;
 
             // Do not search moves with bad enough SEE values (~5 Elo)
