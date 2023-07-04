c3505ce5fc54d78945e16ebddea725776fd6d3e3
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-07-04 20:42:59 +0800

    Remove quiet TTmove extension Bench: 2756596

--- src/search.cpp
+++ src/search.cpp
@@ -1115,13 +1115,6 @@ moves_loop: // When in check, search starts here
           else if (   givesCheck
                    && depth > 9)
               extension = 1;
-
-          // Quiet ttMove extensions (~1 Elo)
-          else if (   PvNode
-                   && move == ttMove
-                   && move == ss->killers[0]
-                   && (*contHist[0])[movedPiece][to_sq(move)] >= 5168)
-              extension = 1;
       }
 
       // Add extension to new depth
