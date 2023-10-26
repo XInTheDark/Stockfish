edb46e4fa052e9cb74d6427708d600253e50bf16
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-26 22:27:58 +0800

    Optimism tuned v2: 97k games Bench: 1096620

--- src/search.cpp
+++ src/search.cpp
@@ -369,8 +369,8 @@ void Thread::search() {
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
             // Adjust optimism based on root move's averageScore (~4 Elo)
-            optimism[us]  = 108 * (avg - 18) / (std::abs(avg - 9) + 110);
-            optimism[~us] = -119 * (avg + 35) / (std::abs(avg - 3) + 112);
+            optimism[us]  = 111 * (avg + 12) / (std::abs(avg + 9) + 118);
+            optimism[~us] = -116 * (avg + 53) / (std::abs(avg + 21) + 114);
 
             // Start with a small aspiration window and, in the case of a fail
             // high/low, re-search with a bigger window until we don't fail
