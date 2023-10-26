edf0ae03fb2d63a743176e6499111015a1ab777c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-26 22:30:24 +0800

    Retry idea from ages ago Bench: 1290530

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ void Thread::search() {
                 // re-search, otherwise exit the loop.
                 if (bestValue <= alpha)
                 {
-                    beta  = (alpha + beta) / 2;
+                    beta  = alpha;
                     alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                     failedHighCnt = 0;
