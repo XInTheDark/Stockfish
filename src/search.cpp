46b1306b1e815a8fa08f83cf3788f1878dd8b39a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-12-01 22:32:43 +0800

    Take 1 fixed Bench: 1440284

--- src/search.cpp
+++ src/search.cpp
@@ -652,7 +652,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
 
         // Partial workaround for the graph history interaction problem
         // For high rule50 counts don't produce transposition table cutoffs.
-        if (pos.rule50_count() < 90 && !(pos.key() & 15))
+        if (pos.rule50_count() < 90 && (pos.key() & 15))
             return ttValue;
     }
 
