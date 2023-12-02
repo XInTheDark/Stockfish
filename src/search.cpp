3ae13fe8185902e830d61925b374745b753c6900
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-12-02 19:12:31 +0800

    Take 3: -4.8% (no weight) Bench: 1344291

--- src/search.cpp
+++ src/search.cpp
@@ -738,7 +738,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
     }
     else
     {
-        ss->staticEval = eval = evaluate(pos, 12);
+        ss->staticEval = eval = evaluate(pos, 0);
         // Save static evaluation into the transposition table
         tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
     }
