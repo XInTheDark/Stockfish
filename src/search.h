a6c3681c127306dd7a9eaeea475d8d6c94664d3c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-02-11 19:01:07 +0800

    Tuned v1: 16k games Bench: 1394175

--- src/search.h
+++ src/search.h
@@ -211,8 +211,8 @@ class Worker {
 
     Depth reduction(bool i, Depth d, int mn, int delta) {
         int reductionScale = reductions[d] * reductions[mn];
-        return (reductionScale + 1177 - int(delta) * 776 / int(rootDelta)) / 1024
-             + (!i && reductionScale > 842);
+        return (reductionScale + 1118 - int(delta) * 793 / int(rootDelta)) / 1024
+             + (!i && reductionScale > 863);
     }
 
     // Get a pointer to the search manager, only allowed to be called by the
