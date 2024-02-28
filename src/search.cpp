d23424f2807b1d809d8afef4b7d50e4f01db2c49
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-02-28 22:15:11 +0800

    Take 1 Bench: 1451753

--- src/search.cpp
+++ src/search.cpp
@@ -1026,7 +1026,7 @@ moves_loop:  // When in check, search starts here
                 && tte->depth() >= depth - 3)
             {
                 Value singularBeta  = ttValue - (60 + 54 * (ss->ttPv && !PvNode)) * depth / 64;
-                Depth singularDepth = newDepth / 2;
+                Depth singularDepth = std::min((depth + 10) * depth / 50, 3 * depth / 5);
 
                 ss->excludedMove = move;
                 value =
