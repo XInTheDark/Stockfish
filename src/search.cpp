61eb05eb87040a7b1d40b2392b9536833b9f6db3
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-05-14 21:36:05 +0800

    Revert depth changes that were introduced in VVLTC tune forever ago
    Bench: 1494747
    
    Link for reference -
    https://tests.stockfishchess.org/tests/view/646628de884ce93b65df2ac9

--- src/search.cpp
+++ src/search.cpp
@@ -836,7 +836,7 @@ Value Search::Worker::search(
     // much above beta, we can (almost) safely prune the previous move.
     probCutBeta = beta + 177 - 65 * improving;
     if (
-      !PvNode && depth > 3
+      !PvNode && depth > 4
       && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
       // If value from transposition table is lower than probCutBeta, don't attempt probCut
       // there and in further interactions with transposition table cutoff depth is set to depth - 3
@@ -892,7 +892,7 @@ moves_loop:  // When in check, search starts here
     // Step 12. A small Probcut idea, when we are in check (~4 Elo)
     probCutBeta = beta + 428;
     if (ss->inCheck && !PvNode && ttCapture && (tte->bound() & BOUND_LOWER)
-        && tte->depth() >= depth - 4 && ttValue >= probCutBeta
+        && tte->depth() >= depth - 3 && ttValue >= probCutBeta
         && std::abs(ttValue) < VALUE_TB_WIN_IN_MAX_PLY && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY)
         return probCutBeta;
 
