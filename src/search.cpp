eeeb13398b56776d3848be3672df2e28973ef0d0
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-05-20 14:46:16 +0800

    high depth reduction rebased Bench: 1156875

--- src/search.cpp
+++ src/search.cpp
@@ -1638,7 +1638,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta,
 
 Depth Search::Worker::reduction(bool i, Depth d, int mn, int delta) {
     int reductionScale = reductions[d] * reductions[mn];
-    return (reductionScale + 1147 - delta * 755 / rootDelta) / 1024 + (!i && reductionScale > 1125);
+    return (reductionScale + 1147 - delta * 755 / rootDelta) / 1024 + (!i && reductionScale > 1125) - (d > 12);
 }
 
 // elapsed() returns the time elapsed since the search started. If the
