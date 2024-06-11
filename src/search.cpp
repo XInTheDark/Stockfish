f0c4a9a58a28cf7823be644e205609b6e5d95504
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-06-11 13:28:40 +0800

    Take 1 Bench: 1531422

--- src/search.cpp
+++ src/search.cpp
@@ -841,13 +841,13 @@ Value Search::Worker::search(
     // much above beta, we can (almost) safely prune the previous move.
     probCutBeta = beta + 177 - 57 * improving;
     if (
-      !PvNode && depth > 3
+      !PvNode && depth > 5
       && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
       // If value from transposition table is lower than probCutBeta, don't attempt probCut
-      // there and in further interactions with transposition table cutoff depth is set to depth - 3
-      // because probCut search has depth set to depth - 4 but we also do a move before it
-      // So effective depth is equal to depth - 3
-      && !(tte->depth() >= depth - 3 && ttValue != VALUE_NONE && ttValue < probCutBeta))
+      // there and in further interactions with transposition table cutoff depth is set to depth - 4
+      // because probCut search has depth set to depth - 5 but we also do a move before it
+      // So effective depth is equal to depth - 4
+      && !(tte->depth() >= depth - 4 && ttValue != VALUE_NONE && ttValue < probCutBeta))
     {
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
@@ -874,7 +874,7 @@ Value Search::Worker::search(
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4,
+                    value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 5,
                                            !cutNode);
 
                 pos.undo_move(move);
@@ -882,7 +882,7 @@ Value Search::Worker::search(
                 if (value >= probCutBeta)
                 {
                     // Save ProbCut data into transposition table
-                    tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 3,
+                    tte->save(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER, depth - 4,
                               move, unadjustedStaticEval, tt.generation());
                     return std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY ? value - (probCutBeta - beta)
                                                                      : value;
